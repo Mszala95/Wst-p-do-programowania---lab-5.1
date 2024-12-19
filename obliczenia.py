470c0d7c6cac4032fd52ab30917c8e6388476654
Mszala95 <140980762+Mszala95@users.noreply.github.com>
2024-12-19 21:52:21 +0100

    Add files via upload

---
+++ obliczenia.py
@@ -0,0 +1,8 @@
+import geometria
+
+promien = 16
+obwod = geometria.obwod_kola(promien)
+pole = geometria.pole_kola(promien)
+
+print(f"Obwód koła o promieniu {promien}: {obwod}")
+print(f"Pole koła o promieniu {promien}: {pole}")
